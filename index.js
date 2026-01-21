f81943ce3b46184a2d0ecf62af059be9b6be82cc
Rene Shi-Strenzke <RShiStrenzke@rosen-group.com>
2026-01-21 09:29:56 +0100

    used st package to serve static files instead of doing it by hand

--- index.js
+++ index.js
@@ -1,58 +1,16 @@
 const https = require('node:https');
 const pem = require('@metcoder95/https-pem');
-const fs = require('fs');
-const path = require('path');
+const st = require('st');
+
+const mount = st({ path: __dirname, url: '/' });
 
 const server = https.createServer(pem, function (req, res) {
-    const url = new URL(req.url, `https://${req.headers.host}`);
-    const pathname = decodeURIComponent(url.pathname);
-    const filePath = path.join(__dirname, '.', pathname);
-
-    if (pathname === '/') {
-        res.writeHead(308, {
-            Location: '/app/index.html'
-        });
-        res.end('Redirecting to /app/index.html');
-        return;
-    }
-
-    const safePath = path.normalize(filePath);
-    if (!safePath.startsWith(__dirname)) {
-        res.write(403);
+    const stHandled = mount(req, res);
+    if (!stHandled) {
+        res.writeHead(403);
         res.end('Forbidden');
         return;
     }
-
-    fs.stat(safePath, (err, stats) => {
-        if (err) {
-            res.statusCode = 404;
-            res.end("404 Not Found");
-            return;
-        }
-
-        if (stats.isFile()) {
-            fs.readFile(safePath, (err, content) => {
-                if (err) {
-                    res.statusCode = 500;
-                    res.end("Internal Server Error");
-                    return;
-                }
-                if (url.href.endsWith('.js'))
-                {
-                    res.setHeader('Content-Type', 'application/javascript');
-                }
-                else
-                {
-                    res.setHeader('Content-Type', 'text/html');
-                }
-
-                res.end(content);
-            });
-        } else {
-            res.statusCode = 403;
-            res.end("403 Forbidden");
-        }
-    });
 });
 
 server.listen(443, function () {
